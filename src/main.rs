4af99ae7501b922ec64868fdc02732f12918b0fc
Brian J Brennan <brianloveswords@gmail.com>
2014-12-22 19:45:54 -0500

    Start making the structures.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
+extern crate serialize;
+
 use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
 use std::thread::Thread;
-
 use std::str;
 use std::os;
 
+use serialize::json;
+
 static DEFAULT_PORT: &'static str = "1469";
+static DEPLOY_VIA: &'static str = "git";
+static DEPLOY_KEY_SRC: &'static str = "/home/robocoup/.ssh/id_rsa";
+
+#[deriving(Decodable, Show)]
+struct RemoteCommand {
+    secret: String,
+    ansible: AnsibleConfig,
+}
+
+#[deriving(Decodable, Show)]
+struct AnsibleConfig {
+    hostname: String,
+    version: String,
+    optional: Option<String>,
+}
+
+#[deriving(Encodable)]
+struct CommandLineVars {
+    hostname: String,
+    deploy_via: String,
+    deploy_version: String,
+    deploy_key_src: String,
+}
+
+impl CommandLineVars {
+    pub fn new(hostname: String, version: String) -> CommandLineVars {
+        CommandLineVars {
+            hostname: hostname,
+            deploy_via: String::from_str(DEPLOY_VIA),
+            deploy_version: version,
+            deploy_key_src: String::from_str(DEPLOY_KEY_SRC),
+        }
+    }
+}
 
 fn get_port() -> String {
     let default_port = String::from_str(DEFAULT_PORT);
@@ -19,31 +56,36 @@ fn main() {
     let address = format!("0.0.0.0:{}", get_port());
     let listener = TcpListener::bind(address.as_slice());
 
-    let mut acceptor = listener.listen();
     println!("Listening at {}", address);
+    let mut acceptor = listener.listen();
 
     fn handle_client(mut stream: TcpStream) {
         let peer_name = stream.peer_name().unwrap();
 
-        stream.set_read_timeout(Some(3000));
+        // Don't leave sockets lying around. If a socket doesn't send
+        // data within 30 seconds, time it out.
+        stream.set_read_timeout(Some(30_000));
 
-        stream.write("~* welcome to the internet *~\n".as_bytes()).ok();
-
-        // Read the incoming message.
-        let msg = match stream.read_to_end() {
+        // Read the incoming bytes.
+        let bytes = match stream.read_to_end() {
             Err(e) => panic!("Error reading incoming message: {}", e),
-            Ok(msg) => msg,
+            Ok(bytes) => bytes,
         };
 
-        // If the message is empty
+        // Turn those bytes into a string.
+        let msg = str::from_utf8(bytes.as_slice()).unwrap();
+
+        // Bail early if we don't have a message to process
         if msg.len() == 0 {
             return
         }
 
-        println!("length of the message: {}", msg.len());
+        let command: RemoteCommand = match json::decode(msg) {
+            Ok(command) => command,
+            Err(e) => panic!("Error converting message to command: {}", e)
+        };
 
-        println!("this is the message: {}",
-                 str::from_utf8(msg.as_slice()).unwrap());
+        println!("{}: {}", peer_name, command);
 
         stream.write("okay, message received".as_bytes()).ok();
 
@@ -53,7 +95,7 @@ fn main() {
 
     for stream in acceptor.incoming() {
         match stream {
-            Err(e) => println!("MAIN: Incoming connection failed: {}", e),
+            Err(e) => panic!("Listening failed: {}", e),
             Ok(stream) => Thread::spawn(move|| {
                 handle_client(stream)
             }).detach(),
