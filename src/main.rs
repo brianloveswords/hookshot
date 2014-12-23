af8c01b179c41bf69adf98be20e4481f09c40090
Brian J Brennan <brianloveswords@gmail.com>
2014-12-23 00:57:27 -0500

    Move `handle_client` out of main
    Also, bail early if the environment isn't up to snuff.

--- src/main.rs
+++ src/main.rs
@@ -74,79 +74,83 @@ fn get_deploy_key_src() -> String {
     }
 }
 
+fn handle_client(mut stream: TcpStream) {
+    let peer_name = stream.peer_name().unwrap();
+    let secret = get_from_env_or_panic(SECRET_ENV_KEY);
+    let playbook = get_from_env_or_panic(PLAYBOOK_ENV_KEY);
+
+    // Don't leave sockets lying around. If a socket doesn't send
+    // data within 30 seconds, time it out.
+    stream.set_read_timeout(Some(30_000));
+
+    // Read the incoming bytes.
+    let bytes = match stream.read_to_end() {
+        Err(e) => panic!("Error reading incoming message: {}", e),
+        Ok(bytes) => bytes,
+    };
+
+    // Bail early if we don't have a message to process
+    if bytes.len() == 0 {
+        return
+    }
+
+    // json::decode requires &str
+    let msg = str::from_utf8(bytes.as_slice()).unwrap();
+
+    // Decode the incoming message or panic
+    let command: RemoteCommandMsg = match json::decode(msg) {
+        Ok(command) => command,
+        Err(e) => {
+            stream.write("error, could not parse message".as_bytes()).ok();
+            panic!("Error converting message to command: {}", e)
+        }
+    };
+
+    if command.secret != secret {
+        stream.write("error, wrong secret".as_bytes()).ok();
+        panic!("Wrong secret");
+    }
+
+    stream.write("okay, message received\n".as_bytes()).ok();
+    println!("{}: {}", peer_name, command);
+
+    let ansible_vars = CommandLineVars::new(command.ansible.hostname,
+                                            command.ansible.version);
+
+    // Start a detached ansible process
+    let mut ansible = Command::new(ANSIBLE_CMD);
+    ansible.detached();
+    ansible.arg("--connection=local");
+    ansible.arg("-i").arg("127.0.0.1,");
+    ansible.arg("-e").arg(json::encode(&ansible_vars));
+    ansible.arg(playbook);
+
+    let result = match ansible.output() {
+        Err(why) => panic!("Could not spawn `ansible-playbook`: {}", why),
+        Ok(output) => output
+    };
+
+    stream.write(result.error.as_slice()).ok();
+    stream.write(result.output.as_slice()).ok();
+    stream.write(format!("{}\n", result.status).as_bytes()).ok();
+
+    println!("{}: Closing connection", peer_name);
+
+    stream.write("okay, see ya later!\n".as_bytes()).ok();
+    drop(stream);
+}
+
 fn main() {
+    // We use these next two lines to panic early if the environment
+    // isn't properly set up.
+    get_from_env_or_panic(SECRET_ENV_KEY);
+    get_from_env_or_panic(PLAYBOOK_ENV_KEY);
+
     let address = format!("0.0.0.0:{}", get_port());
     let listener = TcpListener::bind(address.as_slice());
 
     println!("Listening at {}", address);
     let mut acceptor = listener.listen();
-
-    fn handle_client(mut stream: TcpStream) {
-        let deployer_secret = get_from_env_or_panic(SECRET_ENV_KEY);
-        let playbook = get_from_env_or_panic(PLAYBOOK_ENV_KEY);
-
-        let peer_name = stream.peer_name().unwrap();
-
-        // Don't leave sockets lying around. If a socket doesn't send
-        // data within 30 seconds, time it out.
-        stream.set_read_timeout(Some(30_000));
-
-        // Read the incoming bytes.
-        let bytes = match stream.read_to_end() {
-            Err(e) => panic!("Error reading incoming message: {}", e),
-            Ok(bytes) => bytes,
-        };
-
-        // Bail early if we don't have a message to process
-        if bytes.len() == 0 {
-            return
-        }
-
-        // json::decode requires &str
-        let msg = str::from_utf8(bytes.as_slice()).unwrap();
-
-        // Decode the incoming message or panic
-        let command: RemoteCommandMsg = match json::decode(msg) {
-            Ok(command) => command,
-            Err(e) => {
-                stream.write("error, could not parse message".as_bytes()).ok();
-                panic!("Error converting message to command: {}", e)
-            }
-        };
-
-        if command.secret != deployer_secret {
-            stream.write("error, wrong secret".as_bytes()).ok();
-            panic!("Wrong secret");
-        }
-
-        stream.write("okay, message received\n".as_bytes()).ok();
-        println!("{}: {}", peer_name, command);
-
-        let ansible_vars = CommandLineVars::new(command.ansible.hostname,
-                                                command.ansible.version);
-
-        // Start a detached ansible process
-        let mut ansible = Command::new(ANSIBLE_CMD);
-        ansible.detached();
-        ansible.arg("--connection=local");
-        ansible.arg("-i").arg("127.0.0.1,");
-        ansible.arg("-e").arg(json::encode(&ansible_vars));
-        ansible.arg(playbook);
-
-        let result = match ansible.output() {
-            Err(why) => panic!("Could not spawn `ansible-playbook`: {}", why),
-            Ok(output) => output
-        };
-
-        stream.write(result.error.as_slice()).ok();
-        stream.write(result.output.as_slice()).ok();
-        stream.write(format!("{}\n", result.status).as_bytes()).ok();
-
-        println!("{}: Closing connection", peer_name);
-
-        stream.write("okay, see ya later!\n".as_bytes()).ok();
-        drop(stream);
-    }
 
     for stream in acceptor.incoming() {
         match stream {
