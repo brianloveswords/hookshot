2ef646bdb39150643313de48c2e0fbdb86e62f75
Brian J Brennan <brianloveswords@gmail.com>
2015-09-27 01:04:14 -0400

    Implement wait

--- src/task_manager.rs
+++ src/task_manager.rs
@@ -16,9 +16,6 @@
 //!
 //! - There is no way to signal a shutdown to worker threads.
 //!
-//! - There should be a way to block the main thread so it doesn't end
-//! while there are still active workers.
-//!
 //! # Examples
 //!
 //! ## Waiting for tasks to finish
@@ -62,9 +59,8 @@
 //!
 //! ```
 //! use deployer::task_manager::{TaskManager, Runnable};
-//! use std::thread;
 //!
-//! # fn do_some_hard_work(){ thread::sleep_ms(100); }
+//! # fn do_some_hard_work() { }
 //! struct LongRunningTask {
 //!     result: Option<u64>,
 //! }
@@ -87,8 +83,42 @@
 //! let task_rx = task_manager.add_task("q", LongRunningTask::new()).unwrap();
 //! let task = task_rx.recv().unwrap();
 //! assert_eq!(task.result, Some(42));
-//!```
-
+//! ```
+//!
+//! ## Blocking indefinitely
+//!
+//! ```
+//! use std::sync::{Arc, Mutex};
+//! use deployer::task_manager::{TaskManager, Runnable};
+//! use std::thread;
+//!
+//! # fn do_some_work() { }
+//! struct Task;
+//! impl Runnable for Task {
+//!     fn run(&mut self) { do_some_work() }
+//! }
+//!
+//! let task_manager = Arc::new(Mutex::new(TaskManager::new()));
+//! {
+//!     let shared_manager = task_manager.clone();
+//!     thread::spawn(move || {
+//!         let locked_manager = shared_manager.lock().unwrap();
+//!         task_manager.ensure_queue("q");
+//!         task_manager.add_task("q", Task {});
+//!     })
+//! }
+//!
+//! {
+//!     let shared_manager = task_manager.clone();
+//!     thread::spawn(move || {
+//!         let locked_manager = shared_manager.lock().unwrap();
+//!         task_manager.shutdown_gracefully();
+//!     })
+//! }
+//!
+//! task_manager.wait();
+//! println!("task manager done");
+//! ```
 
 use std::collections::BTreeMap;
 use std::collections::VecDeque;
@@ -119,11 +149,11 @@ impl<T> Queue<T> where T: Runnable + Send {
 type QueueMap<'a, T> = BTreeMap<&'a str, Arc<Mutex<Queue<T>>>>;
 type ThreadMap<'a> = BTreeMap<&'a str, (JoinHandle<()>, Sender<()>)>;
 
-pub struct TaskManager<'a, T>
-    where T: 'static + Runnable + Send {
-        queues: QueueMap<'a, T>,
-        threads: ThreadMap<'a>,
-    }
+pub struct TaskManager<'a, T> where T: 'static + Runnable + Send {
+    queues: QueueMap<'a, T>,
+    threads: ThreadMap<'a>,
+    shutdown_lock: (Sender<()>, Receiver<()>)
+}
 
 #[derive(Debug)]
 pub enum Error {
@@ -137,108 +167,117 @@ impl fmt::Display for Error {
     }
 }
 
-impl<'a, T> TaskManager<'a, T>
-    where T: 'static + Runnable + Send {
-        /// Create a new TaskManager
-        pub fn new() -> TaskManager<'a, T> {
-            TaskManager {
-                queues: QueueMap::<T>::new(),
-                threads: ThreadMap::new(),
-            }
-        }
-
-        /// Add a task to a queue.
-        ///
-        /// # Failures
-        ///
-        /// - `QueueMissing`: could not find the queue. This will only happen if
-        /// an `add_task()` call happens before an `ensure_queue()` call for
-        /// that queue.
-        pub fn add_task(&mut self, queue_key: &'a str, task: T) -> Result<Receiver<T>, Error> {
-            let (task_tx, task_rx) = channel();
-            {
-                let mut locked_queue = match self.queues.get_mut(queue_key) {
-                    // Safe unwrap: With the current implementation it's
-                    // impossible for a lock to get poisoned. There is exactly
-                    // one other spot where we acquire a lock: in the worker
-                    // thread and the only thing we do is `pop_task`, an alias
-                    // for `pop_front` on the underlying VecDeque, which cannot
-                    // cause a thread panic. In this method we only do one thing
-                    // while holding the lock, `push_task` (an alias for
-                    // `push_back`) which also cannot cause a thread panic.
-                    Some(queue_mutex) => queue_mutex.lock().unwrap(),
-                    None => return Err(Error::QueueMissing),
-                };
-                locked_queue.push_task((task, task_tx));
-            }
-
-            // Safe unwrap: If the queue exists, a corresponding thread in the
-            // thread map is guaranteed to exist because both maps are private
-            // and we always create a thread map entry when we create a queue
-            // map entry.
-            let worker_tx = self.get_channel(queue_key).unwrap();
-
-            // Safe unwrap: The worker thread doesn't perform any operations
-            // that can cause a panic.
-            worker_tx.send(()).unwrap();
-
-            Ok(task_rx)
-        }
-
-        /// Create a queue only if one doesn't already exist with that key
-        pub fn ensure_queue(&mut self, queue_key: &'a str) {
-            if self.queues.contains_key(queue_key) { return }
-
-            let queue = Arc::new(Mutex::new(Queue::<T>::new()));
-            self.queues.insert(queue_key, queue);
-
-            // Safe unwrap: We just inserted it above.
-            self.start_worker(queue_key);
-        }
-
-        fn find(&mut self, key: &'a str) -> Option<&mut Arc<Mutex<Queue<T>>>> {
-            self.queues.get_mut(key)
-        }
-        fn get_channel(&self, key: &'a str) -> Option<&Sender<()>> {
-            match self.threads.get(key) {
-                Some(&(_, ref tx)) => Some(tx),
-                None => None,
-            }
-        }
-
-        // TODO: remove once we can use the `result` from the task run.
-        #[allow(unused_must_use)]
-        fn start_worker(&mut self, key: &'a str) {
-            let queue = self.find(key).unwrap().clone();
-            let (worker_tx, worker_rx) = channel();
-            let worker = thread::spawn(move || {
-                loop {
-                    if worker_rx.recv().is_err() {
-                        // This will only happen if the manager gets
-                        // deallocated, which will typically happen if the main
-                        // thread is in the process of shutting down.
-                        break;
-                    }
-
-                    // Safe unwrap: Impossible for lock to get poisoned, see
-                    // comment in `add_task()`.
-                    let possible_task = queue.lock().unwrap().pop_task();
-
-                    if let Some((mut task, task_tx)) = possible_task {
-                        // Protect the worker thread from any panics that would
-                        // be caused by `task.run()`.
-                        thread::spawn(move || {
-                            task.run();
-                            task_tx.send(task);
-                        }).join();
-                    }
-                };
-            });
-            self.threads.insert(key, (worker, worker_tx));
+impl<'a, T> TaskManager<'a, T> where T: 'static + Runnable + Send {
+    /// Create a new TaskManager
+    pub fn new() -> TaskManager<'a, T> {
+        TaskManager {
+            queues: QueueMap::<T>::new(),
+            threads: ThreadMap::new(),
+            shutdown_lock: channel(),
         }
     }
 
+    /// Add a task to a queue. When the task is complete it will be sent over
+    /// the returned `Receiver`.
+    ///
+    /// # Failures
+    ///
+    /// - `QueueMissing`: could not find the queue. This will only happen if
+    /// an `add_task()` call happens before an `ensure_queue()` call for
+    /// that queue.
+    pub fn add_task(&mut self, queue_key: &'a str, task: T) -> Result<Receiver<T>, Error> {
+        let (task_tx, task_rx) = channel();
+        {
+            let mut locked_queue = match self.queues.get_mut(queue_key) {
+                // Safe unwrap: With the current implementation it's
+                // impossible for a lock to get poisoned. There is exactly
+                // one other spot where we acquire a lock: in the worker
+                // thread and the only thing we do is `pop_task`, an alias
+                // for `pop_front` on the underlying VecDeque, which cannot
+                // cause a thread panic. In this method we only do one thing
+                // while holding the lock, `push_task` (an alias for
+                // `push_back`) which also cannot cause a thread panic.
+                Some(queue_mutex) => queue_mutex.lock().unwrap(),
+                None => return Err(Error::QueueMissing),
+            };
+            locked_queue.push_task((task, task_tx));
+        }
 
+        // Safe unwrap: If the queue exists, a corresponding thread in the
+        // thread map is guaranteed to exist because both maps are private
+        // and we always create a thread map entry when we create a queue
+        // map entry.
+        let worker_tx = self.get_channel(queue_key).unwrap();
+
+        // Safe unwrap: The worker thread doesn't perform any operations
+        // that can cause a panic.
+        worker_tx.send(()).unwrap();
+
+        Ok(task_rx)
+    }
+
+    pub fn wait(&self) {
+        let (_, ref rx) = self.shutdown_lock;
+        rx.recv();
+    }
+
+    pub fn shutdown(&self) {
+
+    }
+
+    /// Create a queue only if one doesn't already exist with that key
+    pub fn ensure_queue(&mut self, queue_key: &'a str) {
+        if self.queues.contains_key(queue_key) { return }
+
+        let queue = Arc::new(Mutex::new(Queue::<T>::new()));
+        self.queues.insert(queue_key, queue);
+
+        // Safe unwrap: We just inserted it above.
+        self.start_worker(queue_key);
+    }
+
+    fn find(&mut self, key: &'a str) -> Option<&mut Arc<Mutex<Queue<T>>>> {
+        self.queues.get_mut(key)
+    }
+
+    fn get_channel(&self, key: &'a str) -> Option<&Sender<()>> {
+        match self.threads.get(key) {
+            Some(&(_, ref tx)) => Some(tx),
+            None => None,
+        }
+    }
+
+    // TODO: remove once we can use the `result` from the task run.
+    #[allow(unused_must_use)]
+    fn start_worker(&mut self, key: &'a str) {
+        let queue = self.find(key).unwrap().clone();
+        let (worker_tx, worker_rx) = channel();
+        let worker = thread::spawn(move || {
+            loop {
+                if worker_rx.recv().is_err() {
+                    // This will only happen if the manager gets
+                    // deallocated, which will typically happen if the main
+                    // thread is in the process of shutting down.
+                    break;
+                }
+
+                // Safe unwrap: Impossible for lock to get poisoned, see
+                // comment in `add_task()`.
+                let possible_task = queue.lock().unwrap().pop_task();
+
+                if let Some((mut task, task_tx)) = possible_task {
+                    // Protect the worker thread from any panics that would
+                    // be caused by `task.run()`.
+                    thread::spawn(move || {
+                        task.run();
+                        task_tx.send(task);
+                    }).join();
+                }
+            };
+        });
+        self.threads.insert(key, (worker, worker_tx));
+    }
+}
 
 #[cfg(test)]
 mod tests {
