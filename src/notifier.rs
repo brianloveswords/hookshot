0233f19ffb09188b05e923c6a65efe04885780fc
Brian J Brennan <brianloveswords@gmail.com>
2015-10-21 23:32:06 -0400

    Fix branch lookup; better logging

--- src/notifier.rs
+++ src/notifier.rs
@@ -24,9 +24,13 @@ enum TaskState {
 
 #[allow(unused_must_use)]
 pub fn started(task: &DeployTask, config: &RepoConfig) {
+    println!("[{}] notifier: looking up notify url", &task.id);
     let notify_url = match get_notify_url(task, config) {
         Some(url) => url,
-        None => return,
+        None => {
+            println!("[{}] notifier: could not find notify url", &task.id);
+            return;
+        }
     };
 
     let repo = &task.repo;
@@ -69,8 +73,8 @@ pub fn failed(task: &DeployTask, config: &RepoConfig) {
 }
 
 fn get_notify_url<'a>(task: &DeployTask, config: &'a RepoConfig) -> Option<&'a String> {
-    let fully_qualified_branch = task.repo.fully_qualified_branch();
-    let branch_notify_url = match config.lookup_branch(&fully_qualified_branch) {
+    let branch = &task.repo.branch;
+    let branch_notify_url = match config.lookup_branch(branch) {
         Some(branch) => branch.notify_url.as_ref(),
         None => None,
     };
